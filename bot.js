adea7e86beecbcae666d5f7a476485ffd7c1f71a
iiMohamedYT <45318130+iiMohamedYT@users.noreply.github.com>
2019-07-21 10:47:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "$"
+var prefix = "#"
  
 client.on('message', message => {
     var p = message.mentions.members.first();
@@ -330,4 +330,4 @@ client.on('message', msg => {
    }
    });
 /////
-client.login('NTU4NjMzNzI3MjkxNTU1ODcw.XOtC_A.hKNRUxWxDJCKIPLe6gD_jp1m-NU');
+client.login('NjAyNDIxMTg0MDQ2NDMyMjU3.XTQmeA.nDqdCLDJs9SZPjuYlhXhdbMLyoM');
